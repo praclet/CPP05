86c44e2f3bdd91dc29c5bc3910580ce1d0f52faf
Philippe Raclet <praclet.tel@gmail.com>
2021-04-11 14:12:34 +0200

    Saving files

---
+++ ex03/srcs/PresidentialPardonForm.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.cpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/11 10:34:39 by praclet           #+#    #+#             */
+/*   Updated: 2021/04/11 13:54:04 by praclet          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "PresidentialPardonForm.hpp"
+
+PresidentialPardonForm & PresidentialPardonForm::operator=(PresidentialPardonForm const & src)
+{
+	return (PresidentialPardonForm &)Form::operator=(src);
+}
+
+PresidentialPardonForm::PresidentialPardonForm() : Form()
+{
+}
+
+PresidentialPardonForm::PresidentialPardonForm(PresidentialPardonForm const & src) : Form(src)
+{
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+}
+
+PresidentialPardonForm::PresidentialPardonForm(std::string const & target)
+	: Form("Presidential Pardon", 25, 5, target)
+{
+}
+
+void PresidentialPardonForm::_execute(Bureaucrat const & executor) const
+{
+	(void) executor;
+	std::cout << getTarget() << " has been pardoned by Zafod Beeblebrox."
+		<< std::endl;
+}
