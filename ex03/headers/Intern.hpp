f4e574a0a42fffb0bd2d279bc406e3acbb4e7e10
Philippe Raclet <praclet.tel@gmail.com>
2021-04-12 10:55:32 +0200

    Saving files

--- ex03/headers/Intern.hpp
+++ ex03/headers/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 14:13:10 by praclet           #+#    #+#             */
-/*   Updated: 2021/04/11 18:22:27 by praclet          ###   ########lyon.fr   */
+/*   Updated: 2021/04/12 10:23:44 by praclet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,9 @@
 class Intern
 {
 	private:
-		struct _FormGenerator
-		{
-			std::string const name;
-			Form * (*formCreator)(std::string const &);
-		};
-		static Form * createPPF(std::string const & formName);
-		static Form * createRRF(std::string const & formName);
-		static Form * createSCF(std::string const & formName);
-		static std::size_t const _nbForms = 3;
-		static _FormGenerator const _tabForms[_nbForms] =
-		{
-			{PPF_NAME, Intern::createPPF},
-			{RRF_NAME, &createRRF},
-			{SCF_NAME, &createSCF}
-		};
+		static Form * _createPPF(std::string const & target);
+		static Form * _createRRF(std::string const & target);
+		static Form * _createSCF(std::string const & target);
 	public:
 		Intern();
 		Intern(Intern const & src);
@@ -45,15 +33,3 @@ class Intern
 };
 
 #endif
-
-		/*
-		 * The intern has one important thing, the makeForm function. It takes two strings, the
-first representing the name of a form, and the second one being the target for the form. It
-will return, as a pointer to Form, a pointer to whichever concrete form class is represented
-by the first parameter, initialized with the second parameter. It will print something like
-"Intern creates <form>" to the standard output. If it’s using a worse method, like
-if/elseif/elseif/else branchings, or some other ugly stuff like this, it will not be accepted
-during the evaluation. If the requested form is not known, print an explicit error message.
-
-
-*/
--- ex03/srcs/Intern.cpp
+++ ex03/srcs/Intern.cpp
@@ -6,11 +6,15 @@
 /*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 14:13:09 by praclet           #+#    #+#             */
-/*   Updated: 2021/04/11 14:53:58 by praclet          ###   ########lyon.fr   */
+/*   Updated: 2021/04/12 10:19:29 by praclet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Intern.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 Intern::Intern()
 {
@@ -18,6 +22,7 @@ Intern::Intern()
 
 Intern::Intern(Intern const & src)
 {
+	(void)src;
 }
 
 Intern::~Intern()
@@ -31,5 +36,34 @@ Intern & Intern::operator=(Intern const &)
 
 Form * Intern::makeForm(std::string const & formName, std::string const & targetName)
 {
-	;
+	std::size_t	const nbForms = 3;
+	std::string const formNames[nbForms] = {PPF_NAME, RRF_NAME, SCF_NAME};
+	Form * (*formCreators[nbForms])(std::string const &) =
+		{ &Intern::_createPPF, &Intern::_createRRF, &Intern::_createSCF};
+
+	for (std::size_t i = 0;i < nbForms;i++)
+	{
+		if (formNames[i] == formName)
+		{
+			std::cout << "Intern creates " << formName << "." << std::endl;
+			return (formCreators[i](targetName));
+		}
+	}
+	std::cout << "Intern can't create " << formName << "." << std::endl;
+	return (NULL);
+}
+
+Form * Intern::_createPPF(std::string const & target)
+{
+	return (new PresidentialPardonForm(target));
+}
+
+Form * Intern::_createRRF(std::string const & target)
+{
+	return (new RobotomyRequestForm(target));
+}
+
+Form * Intern::_createSCF(std::string const & target)
+{
+	return (new ShrubberyCreationForm(target));
 }
